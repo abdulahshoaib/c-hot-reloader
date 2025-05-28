fffd7f2f8aba58958d98fe44de52b2f979a91654
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-05-28 17:23:03 +0500

    feat: TUI api header + testing using main

--- inc/ui/ui.h
+++ inc/ui/ui.h
@@ -5,6 +5,7 @@
 void ui_init(void);
 void ui_render_log(const char *msg);
 void ui_render_error(const char *err);
+int ui_getinput(void);
 void ui_cleanup(void);
 
 #endif // !UI_H
--- src/core/main.c
+++ src/core/main.c
@@ -1,3 +1,4 @@
+#include "ui/ui.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,6 +7,18 @@ int main(int argc, char *argv[]) {
     printf("fatal error: no C file attached");
     return EXIT_FAILURE;
   }
+
+  ui_init();
+  ui_render_log("Moniterring Changes...");
+  ui_render_error("error");
+
+  while (1) {
+    if (ui_getinput() == -1)
+      break;
+  }
+
+  ui_cleanup();
+  system("clear");
 
   printf("argument: %s", argv[1]);
 
