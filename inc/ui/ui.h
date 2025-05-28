25a7da1a5bcd79b2ea1d7a41b5405b2c3cc41c91
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-05-28 21:18:26 +0500

    feat: ui logging and outputing for debugging

--- inc/ui/ui.h
+++ inc/ui/ui.h
@@ -5,6 +5,7 @@
 void ui_init(void);
 void ui_render_log(const char *msg);
 void ui_render_log_a(const char *msg, const char *arg);
+void ui_render_log_b(const char *msg, const int arg);
 void ui_render_error(const char *err);
 void ui_render_output(char *output);
 int ui_getinput(void);
